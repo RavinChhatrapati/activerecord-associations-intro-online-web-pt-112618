1bfb9448d9e308b94ee4fc2735ed76439e71c712
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-24 01:42:47 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,5 @@
 class Genre < ActiveRecord::Base
+  has_many :songs
   def song_count
       # return the number of songs in a genre	    # return the number of songs in a genre
       self.songs.count
